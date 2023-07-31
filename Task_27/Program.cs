8331eab0932cc17da484d6066a8343cad7f80eae
Dima Bykadorov <dimka.b@live.com>
2023-07-31 21:38:12 +0300

    add Task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+string num = Console.ReadLine();
+
+Console.WriteLine($"Сумма цифр чила {num} = {GetSumOfDigits(num)}");
+
+int GetSumOfDigits (string number){
+    int sum = 0;
+    for(int i = 0; i < number.Length; i++){
+        sum += int.Parse(number[i].ToString());
+    }
+    return sum;
+}
