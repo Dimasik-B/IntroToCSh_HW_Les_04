cd775970fa2f3743d8692a3570f1c7254df7db1b
Dima Bykadorov <dimka.b@live.com>
2023-07-31 21:18:31 +0300

    add Task 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("В какую степень возвести: ");
+int exp = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"{num} в степени {exp} = {GetExp(num, exp)}");
+
+int GetExp(int a, int b){
+    int result= a;
+    for (int i = 1; i < b; i++){
+        result *= a;
+    }
+    return result;
+}
