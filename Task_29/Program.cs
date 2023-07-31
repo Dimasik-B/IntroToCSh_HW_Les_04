6526e698271c5d75d06a8a39f42aeb2e9e7d2b97
Dima Bykadorov <dimka.b@live.com>
2023-07-31 21:52:11 +0300

    add Task 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите размер массива: ");
+int arraySize = int.Parse(Console.ReadLine());
+Console.Write("Введите минимальное значение элемента массива: ");
+int arrayMin = int.Parse(Console.ReadLine());
+Console.Write("Введите максимальное значение элемента массива: ");
+int arrayMax = int.Parse(Console.ReadLine());
+int[] array = GetRandomArray(arraySize, arrayMin, arrayMax);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] GetRandomArray(int size, int min, int max){
+    int[] arrayTemp = new int[size];
+    for (int i = 0; i < arrayTemp.Length; i++){
+        arrayTemp [i] = new Random().Next(min, max);
+    }
+    return arrayTemp;
+}
